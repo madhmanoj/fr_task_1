92aaa81d4e0fae83c7d964f3b362c1992be1a367
Madhav <madhavmanoj@outlook.com>
2025-04-22 15:12:27 +0000

    Fixing up the run script and the devcontainer

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Clone VSCode if not present
+if [ ! -d "vscode" ]; then
+    git clone https://github.com/microsoft/vscode.git
+    cd vscode
+    git checkout tags/1.99.3
+    npm install
+    npm run compile
+    npm run compile-web
+    cd ..
+fi
+
+./vscode/scripts/code-web.sh --host 0.0.0.0
